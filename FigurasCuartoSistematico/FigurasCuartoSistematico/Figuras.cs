2691c5bcd58b75a4b57a7b75ef481f816002c64d
PaolaUR05 <110620089+PaolaUR05@users.noreply.github.com>
2022-10-02 10:38:26 -0600

    Add files via upload

---
+++ FigurasCuartoSistematico/FigurasCuartoSistematico/Figuras.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigurasCuartoSistematico
+{
+    internal class Figuras : Object
+    {
+        private double area; 
+
+        public Figuras(double area)
+        {
+            this.area = area;
+        } 
+        
+        public double Area
+        {
+            get { return area; }
+           
+        }
+    }
+}
